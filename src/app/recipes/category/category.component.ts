1ae9f025368e4b336c64c6380b0d6c77a2e283bd
anna-lisa1404 <anna-lisa.merkel@fh-erfurt.de>
2023-12-16 21:23:12 +0100

    added dynamic category page (page reload not working)

--- src/app/recipes/category/category.component.ts
+++ src/app/recipes/category/category.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, ParamMap, Router } from '@angular/router';
+import { DataService } from 'src/app/services';
+import { IRecipe } from 'src/app/interfaces';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-category',
@@ -7,13 +10,66 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  currentCategory: string = 'Alle'
+  recipes!: IRecipe[];
 
-  currentCategory: string = ''
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router, private ds: DataService) {
+
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.currentCategory = params['catName']
+      // this.recipes = [];
+      this.currentCategory = params['catName'];
+      console.log(this.currentCategory);
+
+      // todo: funktioniert noch nicht!
+
+      // this.readRecipes(params['catName']);
+      // console.log(this.recipes);
     })
+
+    this.readRecipes(this.currentCategory);
+
+    // this.route.paramMap.subscribe((params: ParamMap) => {
+    //   this.currentCategory = params.get('catName')!;
+    // })
+
+    // this.router.events.subscribe((event) => {
+    //   if(event instanceof NavigationEnd) {
+    //     this.currentCategory = this.route.snapshot.paramMap.get('catName')!;
+    //     console.log(this.currentCategory);
+    //     this.readRecipes(this.currentCategory);
+    //     console.log(this.recipes);  
+    //   }
+    // })
+  }
+
+  readRecipes(category: string): void {
+
+    if(category == "Alle") {
+      this.ds.getAll().subscribe(
+        {
+          next: (response) => {
+            this.recipes = response;
+            // console.log(this.recipes);
+            // return this.recipes;
+          },
+          error: (err) => console.log(err),
+          complete: () => console.log('getAll() completed')
+        })
+    }
+    else {
+      this.ds.getRecipeByCategory(category).subscribe(
+        {
+          next: (response) => {
+            this.recipes = response;
+            // console.log(this.recipes);
+            // return this.recipes;
+          },
+          error: (err) => console.log(err),
+          complete: () => console.log('getByCategory() completed')
+        })
+    }
   }
 }
