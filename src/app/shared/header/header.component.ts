aa8dbb6e708a10fd1d6f5049df2107e32085d8f0
Molham Al khodari <alkhodari@drherwig.de>
2024-01-30 17:55:23 +0100

    homepage

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IUser } from 'src/app/interfaces';
@@ -30,13 +30,33 @@ export class HeaderComponent implements OnInit {
   showSubmenu(): void {
     this.submenuVisible = !this.submenuVisible;
   }
+
   logout() {
     this.authenticationService.logout();
   }
 
   searchRecipes(): void {
     // alert(this.searchForm.controls['recipe'].value);
-    this.router.navigate(['search/' + this.searchForm.controls['recipe']. value]);
+    this.router.navigate(['search/' + this.searchForm.controls['recipe'].value]);
     this.searchForm.reset();
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const header = document.querySelector('header');
+    if (header) {
+      header.classList.toggle("sticky", window.scrollY > 0);
+    }
+  }
+
+  toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    if (menuToggle && navigation) {
+      menuToggle.classList.toggle('active');
+      navigation.classList.toggle('active');
+    }
+  }
+
+
 }
